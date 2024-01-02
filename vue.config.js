ef907a8b75b8f6995c75eb119ae8a6ad31e352b6
Antonina Vitkalova <tonatonotonya@gmail.com>
2024-01-03 01:27:06 +0400

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
-    : '/'
+  outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/search-countries/' : '/'
 }
